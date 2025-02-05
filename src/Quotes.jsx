a64779fecabd44d8a8a1573a874c36d658227d44
avl-vins <avl.vinss@gmail.com>
2025-02-05 15:34:51 -0500

    Agrega archivo QuoteComponent

--- src/Quotes.jsx
+++ src/Quotes.jsx
@@ -1,7 +1,19 @@
+import styled from "styled-components"
+import { QuoteComponent } from "./components/QuoteComponent"
 
+const Container = styled.section`
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    place-content: center;
+`
 
 export const Quotes = () => {
   return (
-    <div>Quotes</div>
+    <Container>
+        <QuoteComponent/>
+    </Container>
+
   )
 }
---
+++ src/components/QuoteComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const QuoteContainer = styled.section`
+    
+`
+export const QuoteComponent = () => {
+  return (
+    <QuoteContainer>
+        <p className="quote__text">asd</p>
+        <h5 className="quote__author">adas</h5>
+
+    </QuoteContainer>
+  )
+}
